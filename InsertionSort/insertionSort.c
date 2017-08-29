586713a756d7aa588a6915de138e4cb8bb8e0bb5
Enver Kayaaslan <ekayaaslan@Envers-MacBook-Pro.local>
2017-08-29 08:26:59 +0200

    insertion sort

---
+++ InsertionSort/insertionSort.c
@@ -0,0 +1,19 @@
+int insertionSort(int items[], int size) {
+  for(int i=1; i<size; i++) {
+    int item = items[i];
+	int isReplaced = 0;
+    for(int j=i; j>0; j--) {
+	  if(item < items[j-1]) {
+	    items[j] = items[j-1];
+	  }else {
+	    items[j] = item;
+		isReplaced = 1;
+	    break;
+	  }
+	}
+	if(!isReplaced) {
+	  items[0] = item;
+	}
+  }
+  return 0;
+}
---
+++ InsertionSort/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int insertionSort(int items[], int size);
+
+int main(int argc, char* argv[]) {
+  FILE *f = fopen("input","r");
+  int size;
+  fscanf(f, "%d", &size);
+  int items[100];
+  for(int i=0; i<size; i++) {
+    fscanf(f, "%d", &items[i]);
+  }
+  insertionSort(items, size);
+  for(int i=0; i<size; i++) {
+    printf("%d ", items[i]);
+  }
+  printf("\n");
+  return 0;
+}
