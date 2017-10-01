f11440923c2c718c29d78cc5c18494883c11b757
Enver Kayaaslan <ekayaaslan@gmail.com>
2017-10-01 02:12:33 +0200

    some clean up

--- KnuthShuffle/shuffle.c
+++
@@ -1,8 +0,0 @@
-void shuffle(int items[], int size, int randoms[]) {
-  for(int i=0; i<size-1; i++) {
-    int item = items[i];
-    int j = i + randoms[i]%(size-i);
-    items[i] = items[j];
-    items[j] = item;
-  }
-}
